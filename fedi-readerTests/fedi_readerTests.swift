4738a53499ef423d2169b065d6530c9945413c02
Sam Clemente <sam@theclementes.com>
2026-01-20 20:53:44 -0600

    Initial Commit

---
+++ fedi-readerTests/fedi_readerTests.swift
@@ -0,0 +1,17 @@
+//
+//  fedi_readerTests.swift
+//  fedi-readerTests
+//
+//  Created by Sam Clemente on 1/20/26.
+//
+
+import Testing
+@testable import fedi_reader
+
+struct fedi_readerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ fedi-readerUITests/fedi_readerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  fedi_readerUITestsLaunchTests.swift
+//  fedi-readerUITests
+//
+//  Created by Sam Clemente on 1/20/26.
+//
+
+import XCTest
+
+final class fedi_readerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
